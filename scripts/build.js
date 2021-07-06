eedf755e608749d9673cb27c1d4031a6ed056e5a
Willy Woitas <willy.woitas@neofonie.de>
2021-07-06 15:53:10 +0200

    add cache buster to css

--- scripts/build.js
+++ scripts/build.js
@@ -1,11 +1,12 @@
 const fs = require('fs-extra');
 const indexPath = './public/index.html';
 const cacheBuster = (new Date().getTime());
-const tpl = `<script defer src="./build/bundle.js?cb=${cacheBuster}"></script>`;
+const tplCSS = `<link rel="stylesheet" href="./build/bundle.css?cb=${cacheBuster}">`;
+const tplJS = `<script defer src="./build/bundle.js?cb=${cacheBuster}"></script>`;
 
 let content = fs.readFileSync(indexPath, 'utf-8');
-
-content = content.replace(/<script defer src="\.\/build\/bundle\.js\?cb=\d*"><\/script>/, tpl);
+content = content.replace(/<link rel="stylesheet" href="\.\/build\/bundle\.css\?cb=\d*">/, tplCSS);
+content = content.replace(/<script defer src="\.\/build\/bundle\.js\?cb=\d*"><\/script>/, tplJS);
 
 fs.writeFileSync(indexPath, content, 'utf-8');
 
