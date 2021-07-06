d1839498669042c0feb600c3ed10cb6c13079948
Willy Woitas <willy.woitas@neofonie.de>
2021-07-06 15:47:40 +0200

    remove flexbox

--- src/App.svelte
+++ src/App.svelte
@@ -2,10 +2,6 @@
 	import Table from "./Table.svelte";
 </script>
 
-<svelte:head>
-	<title>BATB12 - Community Table</title>
-</svelte:head>
-
 <main>
 	<Table />
 </main>
@@ -14,18 +10,10 @@
 	:global(html), :global(body) {
 		height: 100%;
 	}
+
 	:global(body) {
 		margin: 0;
 		padding: 0;
 		background: #050505;
-		display: flex;
-		align-items: center;
-		min-height: 1000px;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
 	}
 </style>
