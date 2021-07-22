419bdbc954fdba7065d9b8ec9634b9389efb1431
Willy Woitas <willy.woitas@neofonie.de>
2021-07-22 20:52:24 +0200

    add cache buster to service worker

--- scripts/build.js
+++ scripts/build.js
@@ -4,10 +4,13 @@ const destPath = './public/index.html';
 const cacheBuster = (new Date().getTime());
 const tplCSS = `<link rel="stylesheet" href="./build/bundle.css?cb=${cacheBuster}">`;
 const tplJS = `<script defer src="./build/bundle.js?cb=${cacheBuster}"></script>`;
+const tplSW = `'./sw.js?cb=${cacheBuster}'`;
 
 let content = fs.readFileSync(srcPath, 'utf-8');
 content = content.replace(/<link rel="stylesheet" href="\.\/build\/bundle\.css\?cb=\d*">/, tplCSS);
 content = content.replace(/<script defer src="\.\/build\/bundle\.js\?cb=\d*"><\/script>/, tplJS);
+content = content.replace(/'\.\/sw\.js\?cb=\d*'/, tplSW);
+// activate tracking
 content = content.replace(/async xsrc/, 'async src');
 
 fs.writeFileSync(destPath, content, 'utf-8');
