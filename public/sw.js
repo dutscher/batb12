a238889ff5e54bd21d82cb183d8967721d9ce9c9
Willy Woitas <willy.woitas@neofonie.de>
2021-07-22 20:40:58 +0200

    add last pwa shizzle

--- public/sw.js
+++ public/sw.js
@@ -1,24 +1,57 @@
-const cacheName = 'batb12';
-const filesToCache = [
-    '/',
-    '/index.html',
-    '/global.css'
+'use strict';
+
+// Update cache names any time any of the cached files change.
+const CACHE_NAME = 'static-cache-v1';
+
+// Add list of files to cache here.
+const FILES_TO_CACHE = [
+    '/offline.html',
 ];
 
-/* Start the service worker and cache all of the app's content */
-self.addEventListener('install', (e) => {
-    e.waitUntil(
-        caches.open(cacheName).then((cache) => {
-            return cache.addAll(filesToCache);
+self.addEventListener('install', (evt) => {
+    console.log('[ServiceWorker] Install');
+
+    evt.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            console.log('[ServiceWorker] Pre-caching offline page');
+            return cache.addAll(FILES_TO_CACHE);
         })
     );
+
+    self.skipWaiting();
 });
 
-/* Serve cached content when offline */
-self.addEventListener('fetch', (e) => {
-    e.respondWith(
-        caches.match(e.request).then((response) => {
-            return response || fetch(e.request);
+self.addEventListener('activate', (evt) => {
+    console.log('[ServiceWorker] Activate');
+    // Remove previous cached data from disk.
+    evt.waitUntil(
+        caches.keys().then((keyList) => {
+            return Promise.all(keyList.map((key) => {
+                if (key !== CACHE_NAME) {
+                    console.log('[ServiceWorker] Removing old cache', key);
+                    return caches.delete(key);
+                }
+            }));
         })
+    );
+
+    self.clients.claim();
+});
+
+self.addEventListener('fetch', (evt) => {
+    console.log('[ServiceWorker] Fetch', evt.request.url);
+    // Add fetch event handler here.
+    if (evt.request.mode !== 'navigate') {
+        // Not a page navigation, bail.
+        return;
+    }
+    evt.respondWith(
+        fetch(evt.request)
+            .catch(() => {
+                return caches.open(CACHE_NAME)
+                    .then((cache) => {
+                        return cache.match('offline.html');
+                    });
+            })
     );
 });
