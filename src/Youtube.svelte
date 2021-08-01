616e9b6454c82dce301a58822441507c33a89e04
Willy Woitas <willy.woitas@neofonie.de>
2021-08-01 10:02:22 +0200

    fix background image src

---
+++ src/Youtube.svelte
@@ -0,0 +1,10 @@
+<script>
+    let activeVideo = '0AIKNWgOo6I'
+</script>
+
+<iframe
+    width="560" height="315"
+    src="https://www.youtube.com/embed/{activeVideo}" title="YouTube video player"
+    frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+    allowfullscreen>
+</iframe>
